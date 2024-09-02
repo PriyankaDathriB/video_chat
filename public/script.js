47f5507b8803b87fb8e6f5f07c8d7b4e959cc027
PriyankaDathriB <priyanka.dathri@gmail.com>
2024-09-02 15:03:35 +0530

    218 WebRtC

--- public/script.js
+++ public/script.js
@@ -1,12 +1,35 @@
 const socket = io("/");
 
-var peer = new Peer(undefined,{
-    path:"/peerjs",
-    host:'/',
-    port:"443",
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
 });
 
-const user = prompt("ENter your name");
+const user = prompt("Enter your name");
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+let myStream;
+
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+    })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        $("#video_grid").append(video)
+    });
+};
 
 $(function () {
     $("#show_chat").click(function () {
@@ -14,26 +37,26 @@ $(function () {
         $(".right-window").css("display", "block")
         $(".header_back").css("display", "block")
     })
-
     $(".header_back").click(function () {
         $(".left-window").css("display", "block")
         $(".right-window").css("display", "none")
         $(".header_back").css("display", "none")
     })
 
-    $("#send").click(function(){
-        if($('#chat_message').val().length!==0){
-            socket.emit("message",$("#chat_message").val());
-            $('#chat_message').val()="";
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
         }
     })
 
-    $('#chat_message').keydown(function(e){
-        if(e.key=="Enter" && $('#chat_message').val().length!==0){
-            socket.emit("message",$("#chat_message").val());
-            $('#chat_message').val()="";
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
         }
     })
+
 })
 
 peer.on("open", (id) => {
